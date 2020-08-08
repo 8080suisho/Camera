df044e24da4857db42f3157b30a50c9f5c3e31b2
Mizuki Morohoshi <8080suisho@gmail.com>
2020-08-09 00:25:45 +0900

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 諸星水晶 on 2020/08/09.
+//  Copyright © 2020 Mizuki Morohoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
